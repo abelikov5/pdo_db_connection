b89e28b3fbf4bed45220997e19cd2bf750b16841
Alexander Belikov <abelikov@inbox.ru>
2020-08-01 20:03:03 +0300

    search at DB

--- index.php
+++ index.php
@@ -18,7 +18,7 @@
 	//установливаем режим выборки данных из db в ввиде ассоциативного массива;
 	$conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
 	//выбираем ВСЁ (все колонки) из таблицы 'posts'
-	$stmt = $conn->query('SELECT * FROM posts');
+
 
 	//Перебираем записи и выводим нужную информацию
 	// while ($res = $stmt->fetch(PDO::FETCH_ASSOC)) {
@@ -52,23 +52,49 @@
 
 /******************************/
 
-//Delete Data from DB;
+//Delete Data at DB;
 
-	$post_id = '7';
+	// $post_id = '8';
 
-	$deleteQuery = "DELETE FROM posts WHERE post_id = :post_id";
-	$del_stmt = $conn->prepare($deleteQuery);
-	$del_stmt->execute(['post_id' => $post_id]);
+	// $deleteQuery = "DELETE FROM posts WHERE post_id = :post_id";
+	// $del_stmt = $conn->prepare($deleteQuery);
+	// $del_stmt->execute(['post_id' => $post_id]);
 
-	if ($deleteQuery) {
-		echo 'Succefull delete data'.'<br>';
-	} else {
-		echo 'Something went wrong into delete data'.'<br>';
-	}
+	// if ($deleteQuery) {
+	// 	echo 'Succefull delete data'.'<br>';
+	// } else {
+	// 	echo 'Something went wrong into delete data'.'<br>';
+	// }
+
+/******************* */
+// Update post at DB
+
+// $post_id = '9';
+// $upd_content = 'Water with OSMO. Wonderfull appartment for rent at Marina Roscha close to city center.';
+
+// $updateQuery = "UPDATE posts SET content = :content WHERE post_id = :post_id";
+// $upd_stmt = $conn->prepare($updateQuery);
+// $upd_stmt->execute(['post_id' => $post_id, 'content' => $upd_content]);
+
+// if ($updateQuery) {
+// 	echo 'Succefull update data'.'<br>';
+// } else {
+// 	echo 'Something went wrong into update data'.'<br>';
+// }
+
+/******************* */
+// Search at DB
+
+$title = '%learn php%';
+
+$search_stmt = 'SELECT * FROM posts WHERE title LIKE :content';
+$stmt = $conn->prepare($search_stmt);
+$stmt->execute(['content' => $title]);
+
 
 //Перебираем записи и выводим нужную информацию
 	while ($res = $stmt->fetch(PDO::FETCH_ASSOC)) {
-		echo 'Post number: '.$res['post_id'].': title: '.$res['title'].'<br>';
+		echo 'Post number: '.$res['post_id'].': title: '.$res['title'].' '.$res['content'].'<br>';
 	}
 
 ?>
