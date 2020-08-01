20029d84a51c357f1f5b33c141f543273217e858
Alexander Belikov <abelikov@inbox.ru>
2020-08-01 19:06:18 +0300

    delete data from DB

--- index.php
+++ index.php
@@ -25,33 +25,50 @@
 	// 	echo 'Post number: '.$res['post_id'].': title: '.$res['title'].'<br>';
 	// }
 	//Перебираем записи и выводим нужную информацию согласно определенным параметрам (user_id);
-	$user_id = 2;
-	$stmt = $conn->query("SELECT * FROM posts WHERE user_id = '$user_id'");
-	while ($res = $stmt->fetch(PDO::FETCH_ASSOC)) {
-		echo 'Post number: '.$res['post_id'].': title: '.$res['title'].'<br>';
-	}
+	// $user_id = 2;
+	// $stmt = $conn->query("SELECT * FROM posts WHERE user_id = '$user_id'");
+	// while ($res = $stmt->fetch(PDO::FETCH_ASSOC)) {
+	// 	echo 'Post number: '.$res['post_id'].': title: '.$res['title'].'<br>';
+	// }
 
 	/******************************/
 	//Insert data to DB
 	//На основании именнованных параметров
 
-	$title = 'I have an appartment';
-	$cat_id = '1';
-	$content = 'Wonderfull appartment for rent at Marina Roscha close to city center';
-	$user_id = '1';
+	// $title = 'I have an appartment';
+	// $cat_id = '1';
+	// $content = 'Wonderfull appartment for rent at Marina Roscha close to city center';
+	// $user_id = '1';
 
-	$insertQuery = "INSERT INTO posts (title, cat_id, content, user_id) VALUE(:title, :cat_id, :content, :user_id)";
-	$stmt = $conn->prepare($insertQuery);
-	$stmt->execute(['title' => $title, 'cat_id' => $cat_id, 'content' => $content, 'user_id' => $user_id]);
+	// $insertQuery = "INSERT INTO posts (title, cat_id, content, user_id) VALUE(:title, :cat_id, :content, :user_id)";
+	// $insert_stmt = $conn->prepare($insertQuery);
+	// $insert_stmt->execute(['title' => $title, 'cat_id' => $cat_id, 'content' => $content, 'user_id' => $user_id]);
 
-	if ($insertQuery) {
-		echo 'Succefull insert';
+	// if ($insertQuery) {
+	// 	echo 'Succefull insert'.'<br>';
+	// } else {
+	// 	echo 'Something went wrong'.'<br>';
+	// }
+
+/******************************/
+
+//Delete Data from DB;
+
+	$post_id = '7';
+
+	$deleteQuery = "DELETE FROM posts WHERE post_id = :post_id";
+	$del_stmt = $conn->prepare($deleteQuery);
+	$del_stmt->execute(['post_id' => $post_id]);
+
+	if ($deleteQuery) {
+		echo 'Succefull delete data'.'<br>';
 	} else {
-		echo 'Something went wrong';
+		echo 'Something went wrong into delete data'.'<br>';
 	}
 
-
-
-
+//Перебираем записи и выводим нужную информацию
+	while ($res = $stmt->fetch(PDO::FETCH_ASSOC)) {
+		echo 'Post number: '.$res['post_id'].': title: '.$res['title'].'<br>';
+	}
 
 ?>
